7576cdb410c14c91bbe5af0d9b95fc99798bfb5c
Manuel <C0526598@outlook.com>
2024-03-04 21:57:22 -0800

    The teams are comparing one another

--- input_testing.py
+++ input_testing.py
@@ -4,25 +4,35 @@ import random
 # Friday = 6
 # No Preference = 8
 fixed_inputs = {
-    0: [0, 'No Preference', 0],
-    1: [1, 'Friday', 0],
-    2: [2, 'No Preference', 0],
-    3: [3, 'Tuesday', 0],
-    4: [4, 'No Preference', 0],
-    5: [5, 'Friday', 0],
-    6: [6, 'No Preference', 0],
-    7: [7, 'No Preference', 0],
-    8: [8, 'No Preference', 0],
-    9: [9, 'Friday', 0],
-    10: [10, 'Friday', 0],
-    11: [11, 'Tuesday', 0],
-    12: [12, 'No Preference', 0],
-    13: [13, 'Friday', 0],
-    14: [14, 'Tuesday', 0],
-    15: [15, 'No Preference', 0],
-    16: [16, 'Friday', 0],
-    17: [17, 'Tuesday', 0]
+    0: [0, 'No Preference', 0, []],
+    1: [1, 'Friday', 0, []],
+    2: [2, 'No Preference', 0, []],
+    3: [3, 'Tuesday', 0, []],
+    4: [4, 'No Preference', 0, []],
+    5: [5, 'Friday', 0, []],
 }
+
+
+# fixed_inputs = {
+#     0: [0, 'No Preference', 0, []],
+#     1: [1, 'Friday', 0, []],
+#     2: [2, 'No Preference', 0, []],
+#     3: [3, 'Tuesday', 0, []],
+#     4: [4, 'No Preference', 0, []],
+#     5: [5, 'Friday', 0, []],
+#     6: [6, 'No Preference', 0, []],
+#     7: [7, 'No Preference', 0, []],
+#     8: [8, 'No Preference', 0, []],
+#     9: [9, 'Friday', 0, []],
+#     10: [10, 'Friday', 0, []],
+#     11: [11, 'Tuesday', 0, []],
+#     12: [12, 'No Preference', 0, []],
+#     13: [13, 'Friday', 0, []],
+#     14: [14, 'Tuesday', 0, []],
+#     15: [15, 'No Preference', 0, []],
+#     16: [16, 'Friday', 0, []],
+#     17: [17, 'Tuesday', 0, []]
+# }
 
 
 number_teams = 18
--- main.py
+++ main.py
@@ -1,54 +1,60 @@
 import input_testing
 
 
-# Compare if the teams are equal
-# def compare_teams(my_dict):
-#     number_teams = 18
-#     number_weeks = 16
-#
-#     for i in range(number_teams):
-#         for j in range(i + 1, number_teams):
-#             if my_dict[i][0] == my_dict[j][0]:
-#                 my_dict[i][1] += 1
-#                 my_dict[j][1] += 1
-
-
 # Match the teams to there preference
-def matching_teams(my_dict) -> True:
-    return False
+def matching_teams(sorted_dict):
+    preference_list = ['Tuesday', 'Friday', 'No Preference']
+    seen_numbers = []
 
+    for group in sorted_dict:
+        for i in range(len(group)):
+            current_team = group[i]
+            for j in range(i + 1, len(group)):
+                compared_team = group[j]
+                print(f"{current_team[0]} vs {compared_team[0]}")
+                seen_numbers.append(compared_team[0])
+            current_team[3].extend(seen_numbers)
+            seen_numbers.clear()
+
+
+def group_team_preference(my_dict):
+    preference_list = ['Tuesday', 'Friday', 'No Preference']
+    groups = {preference: [] for preference in preference_list}
+
+    for team in my_dict.values():
+        preference = team[1]
+        groups[preference].append(team)
+
+    return groups.values()
 
 # Group the teams into there preference
-def group_team_preference(my_dict) :
-    preference_list = ['Tuesday', 'Friday', 'No Preference']
-    group_a = []
-    group_b = []
-    group_c = []
-
-    for x in range(input_testing.number_teams):
-        if my_dict[x][1] == preference_list[0]:
-            group_a.append(my_dict[x])
-        elif my_dict[x][1] == preference_list[1]:
-            group_b.append(my_dict[x])
-        else:
-            group_c.append(my_dict[x])
-
-    # for x in range(len(group_A)):
-    #     print(group_A[x])
+# def group_team_preference(my_dict):
+#     preference_list = ['Tuesday', 'Friday', 'No Preference']
+#     group_a = []
+#     group_b = []
+#     group_c = []
+#
+#     for x in range(input_testing.number_teams):
+#         if my_dict[x][1] == preference_list[0]:
+#             group_a.append(my_dict[x])
+#         elif my_dict[x][1] == preference_list[1]:
+#             group_b.append(my_dict[x])
+#         else:
+#             group_c.append(my_dict[x])
+#
+#     return_groups = [group_a, group_b, group_c]
+#
+#     return return_groups
 
 
 def start():
     the_dict = input_testing.fixed_inputs
+    sorted_groups = group_team_preference(the_dict)
+    matching_teams(sorted_groups)
 
     # Print out the dictionary
-    for key, value in the_dict.items():
-        print(f"{key}: {value[0]}, {value[1]}, {value[2]} ")
-
-    # Compare with days the teams is playing on there preferred day
-    # compare_teams(my_dict)
-    # matching_teams(my_dict)
-    #group_team_preference(my_dict)
-
+    # for key, value in the_dict.items():
+    #     print(f"{key}: {value[0]}, {value[1]}, {value[2]} ")
 
 
 start()
